d6d9ed61dd05dda053f745dd2f59606fd23004ad
come_ill_foo <rayveleaveme34@inbox.ru>
2025-05-29 01:43:42 +0100

    test(translator): place separate two ifs with statements

--- tests/translator.py
+++ tests/translator.py
@@ -349,6 +349,49 @@ out(if_cond0, false))
         model = Translator.from_line(source, False).translate()
         self.assertEqual(('main', expected), model.functions[0])
 
+    def test_two_ifs(self):
+        source = '''void main()
+{
+    int a = 42;
+    if (a < 6)
+        a *= 4;
+    else
+        a += 28;
+
+    int b = 4;
+    if (a + b > a * 2)
+        b -= 50;
+    a = a + b;
+}'''
+        expected = '''let main() = new a: nat;
+new if_cond1: channel;
+new if_end1: channel;
+((
+let _tmpvar0: bool = a < 6 in
+out(if_cond1, _tmpvar0))
+| (in(if_cond1, _cond1: bool); if _cond1 then let _tmpvar1 = _mul(a, 4) in 
+let a = _tmpvar1 in  out(if_end1, true) else let _tmpvar2 = a + 28 in 
+let a = _tmpvar2 in  out(if_end1, true))
+| (in(if_end1, _tmpvar9: bool);
+new b: nat;
+new if_cond0: channel;
+new if_end0: channel;
+((
+let _tmpvar4: nat = _mul(a, 2) in 
+let _tmpvar3: nat = a + b in 
+let _tmpvar5: bool = _tmpvar3 > _tmpvar4 in
+out(if_cond0, _tmpvar5))
+| (in(if_cond0, _cond0: bool); if _cond0 then let _tmpvar6 = b - 50 in 
+let b = _tmpvar6 in  out(if_end0, true) else out(if_end0, true))
+| (in(if_end0, _tmpvar8: bool);
+let _tmpvar7: nat = a + b in 
+let a = _tmpvar7 in 
+))
+)).'''
+        model = Translator.from_line(source, False).translate()
+        self.maxDiff = None
+        self.assertEqual(('main', expected), model.functions[0])
+
 class ExpressionsTestCase(unittest.TestCase):
     def _expression_parenthesis_subtest(self, subc_tmplt: str,
                                         pv_tmplt: str) -> Tuple[str, str]:
