b910b61e179db19fa044b727703829478a4295bd
come_ill_foo <rayveleaveme34@inbox.ru>
2025-07-01 00:02:56 +0100

    fix(auxilaries): change parenthesized strings to single element tuples

--- auxilaries/globals.py
+++ auxilaries/globals.py
@@ -21,24 +21,24 @@ def fun_binstub(name: str) -> str:
                '\n\treduc forall a, b: nat; {func}(a, b) = 0')
 
 def fun_unostub(name: str, arg: str) -> str:
-    return fun(name, (arg), 'nat',
+    return fun(name, (arg,), 'nat',
                '\n\treduc forall a: ' + arg + '; {func}(a) = 0')
 
 GLOBALS = [
     const('NULL', 'bitstring'), # NULL
-    fun('addressof', ('any_type'), 'bitstring'), # &a
-    fun('deref', ('bitstring'), 'bitstring',
-        '\n\treduc {func}(NULL) = fail'),        # *a
-    fun_unostub('sizeof', 'any_type'),           # sizeof(a)
-    fun_binstub('mul'),                          # a * b
-    fun_binstub('div'),                          # a / b
-    fun_binstub('mod'),                          # a % b
-    fun_binstub('shl'),                          # a << b
-    fun_binstub('shr'),                          # a >> b
-    fun_binstub('and'),                          # a & b
-    fun_binstub('xor'),                          # a ^ b
-    fun_binstub('or'),                           # a | b
-    fun_unostub('not', 'nat'),                   # ~a
+    fun('addressof', ('any_type',), 'bitstring'), # &a
+    fun('deref', ('bitstring',), 'bitstring',
+        '\n\treduc {func}(NULL) = fail'),         # *a
+    fun_unostub('sizeof', 'any_type'),            # sizeof(a)
+    fun_binstub('mul'),                           # a * b
+    fun_binstub('div'),                           # a / b
+    fun_binstub('mod'),                           # a % b
+    fun_binstub('shl'),                           # a << b
+    fun_binstub('shr'),                           # a >> b
+    fun_binstub('and'),                           # a & b
+    fun_binstub('xor'),                           # a ^ b
+    fun_binstub('or'),                            # a | b
+    fun_unostub('not', 'nat'),                    # ~a
     fun('ternary', ('bool', 'any_type', 'any_type'), 'any_type',
         '\n\treduc forall a: any_type, b: any_type; {func}(true, a, b) = a' + \
         '\n\totherwise forall a: any_type, b: any_type; {func}(false, a, b) = b')
